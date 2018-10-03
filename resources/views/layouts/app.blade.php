41f423391ef597efb616aa997208054d56671ed6
vagrant <vagrant@homestead>
2018-10-03 10:34:49 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">    
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
         <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">    
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    @yield('styles')    
 </head>
 
 <body>
@@ -28,5 +29,6 @@
 
     <!-- Scripts -->
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
